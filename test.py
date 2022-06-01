6c02512d3a4d2df5a15ba36e538688fe090c6948
fakeuser <fakemail@mail.com>
2022-05-31 23:02:59 -0300

    31/5/22

--- test.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-
-# Libreria
-print("Active development for Minecraft 1.18.2")
-spigot_select = int(input("[1]1.18.2\n[2]1.18.1\n[3]1.17.1\n..."))
-
-resp = requests.get(f'http://localhost:80/spigot/{spigot_select}')
-print(resp.json()["link"])
